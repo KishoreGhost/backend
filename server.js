ce9afd990bd93cc7f99073c8d8f116a2adf0d04f
Kishore <ki225ku@gmail.com>
2024-08-07 13:09:11 +0530

    done

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6969;
 const connectToDB = require("./database");
 
 app.use(express.Router());
