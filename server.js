5b32a68e5d33d102a260230932975800f10ba415
Kishore <ki225ku@gmail.com>
2024-08-07 12:17:18 +0530

    backend setup done

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+const connectToDB = require("./database");
+
+app.use(express.Router());
+app.use(cors());
+
+// connectToDB(); P.S: Uncomment after adding the mongoURI
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
